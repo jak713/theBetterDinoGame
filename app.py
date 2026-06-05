446c31aefa9f34b10859ac6ab8ce0ef7be08c53a
BoonChinLook <lookbc30@gmail.com>
2026-06-05 16:55:40 +0100

    add hobby details to app.py for Boon-Chin

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 # Name: May
 # Hobby: Reading
 # Why I enjoy it: I find reading a great way to switch off after a busy day at work.
+
+# Name: Boon Chin
+# Hobby: Sleeping
+# Why I enjoy it: I like it as it gives my body rest! and I like cats too!
