0e43751560501a7b07cae59dc10f439d0bf02926
jak713 <juliakaczmarek00@gmail.com>
2026-08-15 14:24:17 +0100

    sqlite3 migration continued

--- src/backend/config_template.py
+++
@@ -1,9 +0,0 @@
-# Add MySQL password below and rename file to config.py
-# config.py has been added to gitignore
-
-db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "",  # to update
-    "database": "",  # to update
-}
